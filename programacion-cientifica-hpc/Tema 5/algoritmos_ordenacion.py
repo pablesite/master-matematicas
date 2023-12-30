3e559ef83347ef1fca8cdcf43eff9c84850b2fcf
Pablo Ruiz <pablo.ruiz.molina@gmail.com>
2023-12-30 12:22:32 +0100

    Método de ordenación por selección

---
+++ programacion-cientifica-hpc/Tema 5/algoritmos_ordenacion.py
@@ -0,0 +1,23 @@
+""" Algoritmos de ordenación"""
+
+def ordenacion_burbuja(lista, num_comparaciones):
+    """ Algoritmo que ordena valores en una lista con el método de la burbuja """
+    n = len(lista)
+    for i in range(1, n):
+        for j in range(n - i):
+            num_comparaciones += 1
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista [j + 1], lista[j]
+    return num_comparaciones
+
+def ordenacion_seleccion(lista, num_comparaciones):
+    """ Algoritmo que ordena valores en una lista con el método de selección """
+    n = len(lista)
+    for i in range(n - 1):
+        minimo = i
+        for j in range(i + 1, n):
+            num_comparaciones += 1
+            if lista[j] < lista[minimo]:
+                minimo = j
+        lista[i], lista[minimo] = lista [minimo], lista[i]
+    return num_comparaciones
--- programacion-cientifica-hpc/Tema 5/test_ordenacion.py
+++ programacion-cientifica-hpc/Tema 5/test_ordenacion.py
@@ -1,7 +1,7 @@
 """ Módulo de test para probar los diferentes métodos de ordenación """
 
 from time import time
-from ordenacion_burbuja import ordenacion_burbuja
+from algoritmos_ordenacion import ordenacion_burbuja, ordenacion_seleccion
 
 # Ordenación de burbuja.
 lista = [35, 36, 17, 73, 8, 0]
@@ -9,6 +9,19 @@ NUM_COMPARACIONES = 0
 t0 = time()
 NUM_COMPARACIONES = ordenacion_burbuja(lista, NUM_COMPARACIONES)
 t1 = time()
+print("\n Método de ordenación de la burbuja")
+print("Lista ordenada: " + str(lista))
+print("Tiempo: {0:f} segundos" .format(t1 - t0))
+print("Comparaciones:", NUM_COMPARACIONES)
+
+
+# Ordenación de burbuja.
+lista = [35, 36, 16, 17, 73, 8, 0, 50, 62, 4]
+NUM_COMPARACIONES = 0
+t0 = time()
+NUM_COMPARACIONES = ordenacion_seleccion(lista, NUM_COMPARACIONES)
+t1 = time()
+print("\n Método de ordenación de selección")
 print("Lista ordenada: " + str(lista))
 print("Tiempo: {0:f} segundos" .format(t1 - t0))
 print("Comparaciones:", NUM_COMPARACIONES)
