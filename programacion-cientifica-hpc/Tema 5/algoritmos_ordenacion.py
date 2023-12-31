349c1314f71c4f160f584ba0058ba9194f143dca
Pablo Ruiz <pablo.ruiz.molina@gmail.com>
2023-12-31 08:41:19 +0100

    Método de ordenación por mezcla (recursivo)

--- programacion-cientifica-hpc/Tema 5/algoritmos_ordenacion.py
+++ programacion-cientifica-hpc/Tema 5/algoritmos_ordenacion.py
@@ -1,4 +1,5 @@
 """ Algoritmos de ordenación"""
+import math
 
 def ordenacion_burbuja(lista, num_comparaciones):
     """ Algoritmo que ordena valores en una lista con el método de la burbuja """
@@ -34,3 +35,38 @@ def ordenacion_insercion(lista, num_comparaciones):
             num_comparaciones += 1
         lista[j] = val
     return num_comparaciones
+
+def ordenacion_mergesort(lista, num_comparaciones):
+    """ Algoritmo que ordena valores con la estrategia de ordenación por mezcla """
+    if len(lista) <= 1:
+        return lista, num_comparaciones
+
+    medio = len(lista) // 2
+    izquierda = lista[:medio]
+    derecha = lista[medio:]
+
+    izquierda, num_comparaciones = ordenacion_mergesort(izquierda, num_comparaciones)
+    derecha, num_comparaciones = ordenacion_mergesort(derecha, num_comparaciones)
+
+    lista, num_comparaciones = merge(izquierda, derecha, num_comparaciones)
+    return lista , num_comparaciones
+
+def merge(lista_a, lista_b, num_comparaciones):
+    """ Mecla del algoritmo de ordenación por mezcla """
+    lista = [math.inf for _ in range (len(lista_a) + len(lista_b))]
+
+    lista_a.append(math.inf)
+    lista_b.append(math.inf)
+
+    i = 0
+    j = 0
+
+    for k in range(0, len(lista)):
+        num_comparaciones += 1
+        if lista_a[i] < lista_b[j]:
+            lista[k] = lista_a[i]
+            i += 1
+        else:
+            lista[k] = lista_b[j]
+            j += 1
+    return lista, num_comparaciones
--- programacion-cientifica-hpc/Tema 5/test_ordenacion.py
+++ programacion-cientifica-hpc/Tema 5/test_ordenacion.py
@@ -1,10 +1,10 @@
 """ Módulo de test para probar los diferentes métodos de ordenación """
 
 from time import time
-from algoritmos_ordenacion import ordenacion_burbuja, ordenacion_seleccion, ordenacion_insercion
+from algoritmos_ordenacion import ordenacion_burbuja, ordenacion_seleccion, ordenacion_insercion, ordenacion_mergesort
 
 # Ordenación de burbuja.
-lista = [35, 36, 17, 73, 8, 0]
+lista = [35, 36, 16, 17, 73, 8, 0, 27, 49, 50, 62, 15, 4, 3, 47, 24, 12, 67, 324, 10, 1, 45, 7, 9, 5, 2]
 NUM_COMPARACIONES = 0
 t0 = time()
 NUM_COMPARACIONES = ordenacion_burbuja(lista, NUM_COMPARACIONES)
@@ -14,8 +14,8 @@ print("Lista ordenada: " + str(lista))
 print("Tiempo: {0:f} segundos" .format(t1 - t0))
 print("Comparaciones:", NUM_COMPARACIONES)
 
-# Ordenación de burbuja.
-lista = [35, 36, 16, 17, 73, 8, 0, 50, 62, 4]
+# Ordenación por selección.
+lista = [35, 36, 16, 17, 73, 8, 0, 27, 49, 50, 62, 15, 4, 3, 47, 24, 12, 67, 324, 10, 1, 45, 7, 9, 5, 2]
 NUM_COMPARACIONES = 0
 t0 = time()
 NUM_COMPARACIONES = ordenacion_seleccion(lista, NUM_COMPARACIONES)
@@ -25,13 +25,24 @@ print("Lista ordenada: " + str(lista))
 print("Tiempo: {0:f} segundos" .format(t1 - t0))
 print("Comparaciones:", NUM_COMPARACIONES)
 
-# Ordenación de burbuja.
-lista = [35, 36, 17, 73, 8, 0]
+# Ordenación de insercción.
+lista = [35, 36, 16, 17, 73, 8, 0, 27, 49, 50, 62, 15, 4, 3, 47, 24, 12, 67, 324, 10, 1, 45, 7, 9, 5, 2]
 NUM_COMPARACIONES = 0
 t0 = time()
 NUM_COMPARACIONES = ordenacion_insercion(lista, NUM_COMPARACIONES)
 t1 = time()
-print("\n Método de ordenación de selección")
+print("\n Método de ordenación por inserción")
+print("Lista ordenada: " + str(lista))
+print("Tiempo: {0:f} segundos" .format(t1 - t0))
+print("Comparaciones:", NUM_COMPARACIONES)
+
+# Ordenación por mezcla (recursivo).
+lista = [35, 36, 16, 17, 73, 8, 0, 27, 49, 50, 62, 15, 4, 3, 47, 24, 12, 67, 324, 10, 1, 45, 7, 9, 5, 2]
+NUM_COMPARACIONES = 0
+t0 = time()
+lista, NUM_COMPARACIONES = ordenacion_mergesort(lista, NUM_COMPARACIONES)
+t1 = time()
+print("\n Método de ordenación por mezcla")
 print("Lista ordenada: " + str(lista))
 print("Tiempo: {0:f} segundos" .format(t1 - t0))
 print("Comparaciones:", NUM_COMPARACIONES)
