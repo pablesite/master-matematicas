66052637783e6d179b0030827ec9c25672f283ce
Pablo Ruiz <pablo.ruiz.molina@gmail.com>
2024-01-01 20:43:45 +0100

    Algoritmo de búsqueda recursiva

--- programacion-cientifica-hpc/Tema 5/algoritmos_busqueda.py
+++ programacion-cientifica-hpc/Tema 5/algoritmos_busqueda.py
@@ -6,3 +6,24 @@ def busqueda_secuencial(lista, x):
         if lista[i] == x:
             return i
     return -1
+
+
+def busqueda_recursiva(lista, x):
+    """ Algoritmo que busca un elemento de manera recursiva en una lista ordenada """
+    if len(lista) == 0:
+        return -1
+    else:
+        return busqueda_binaria_rec(lista, x, 0, len(lista) - 1)
+    
+def busqueda_binaria_rec(lista, x, primero, ultimo):
+    print(lista)
+    if primero > ultimo:
+        return -1
+    medio = (primero + ultimo) // 2
+    print(medio)
+    if lista[medio] < x:
+        return busqueda_binaria_rec(lista, x, medio + 1, ultimo)
+    elif lista[medio] > x:
+        return busqueda_binaria_rec(lista, x, primero, medio - 1)
+    else:
+        return medio
--- programacion-cientifica-hpc/Tema 5/test_búsqueda.py
+++ programacion-cientifica-hpc/Tema 5/test_búsqueda.py
@@ -1,13 +1,25 @@
 """ Módulo de test para probar los diferentes algoritmos de búsqueda """
 
 from time import time
-from algoritmos_busqueda import busqueda_secuencial
+from algoritmos_busqueda import busqueda_secuencial, busqueda_recursiva
 
 # Búsqueda secuencial
 lista = [35, 36, 16, 17, 73, 8, 0, 27, 49, 50, 62, 15, 4, 3, 47, 24, 12, 67, 324, 10, 1, 45, 7, 9, 5, 2]
 print("\n Método de búsqueda secuencial")
 t0 = time()
 indice = busqueda_secuencial(lista, 49)
+t1 = time()
+
+print("Índice encontrado: " + str(indice))
+print("Tiempo: {0:f} segundos" .format(t1 - t0))
+
+
+# Búsqueda recursiva
+lista = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 12, 15, 16, 17, 24, 27, 35, 36, 45, 47, 49, 50, 62, 67, 73, 324]
+#lista = [0,  1, 2, 3, 4]
+print("\n Método de búsqueda recursiva")
+t0 = time()
+indice = busqueda_recursiva(lista, 73)
 t1 = time()
 
 print("Índice encontrado: " + str(indice))
