e3ffe1d9642e19a8e9f06003ac0dd05ee851c612
Pablo Ruiz <pablo.ruiz.molina@gmail.com>
2024-01-28 10:50:58 +0100

    Añado funciones de Runge-Kutta y otras

---
+++ metodos-numericos-1/Tema 6/Euler_Sistemas.m
@@ -0,0 +1,16 @@
+function [t, y] = Euler_Sistemas(f, a, b, N, Ya)
+% Código para resolver un PVI con el método de Euler en un sistema
+    h = (b - a)/N;
+    t = a:h:b;
+    t = t(:);
+    Y = zeros(N + 1, length(Ya));
+    Y(1,:) = Ya;
+    for k = 1:N
+        Y(k + 1, :) = Y(k,:) + h*feval(f, t(k), Y(k,:)); 
+        % Este último vector será traspuesto o no dependiendo cómo defina luego las funciones del sistema.
+        % Si las defino en de tipo fila (con ';', añadiendo columnas para cada
+        % variable del sistema), entonces sí que sería traspuesta. Pero si
+        % las defino de tipo columna (con ',', todas las variables del sistema en la misma columna)
+        % creo que no debería hacer falta trasponer. COMPROBAR
+    end
+end
