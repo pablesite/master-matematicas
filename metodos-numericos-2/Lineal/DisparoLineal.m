4d34b39aa6df341b079ba3a5686b9f5cff267c18
Pablo Ruiz <pablo.ruiz.molina@gmail.com>
2024-04-13 18:01:17 +0200

    MN-2 Primeros ejercicios disparo lineal

--- metodos-numericos-2/Lineal/DisparoLineal.m
+++ metodos-numericos-2/Lineal/DisparoLineal.m
@@ -1,10 +1,17 @@
 function [x,y,Y1,Y2]=DisparoLineal(PVI1,PVI2,a,b,n,alfa,beta)
-h=(b-a)/n;
-x=a:h:b;
-x=x(:);
+    % PVI1 y PVI2 son archivos .m donde definimos las funciones que
+    % describen los dos problemas de valor incial.
+    h=(b-a)/n;
+    x=a:h:b;
+    x=x(:);
 
-[x,Y1]=ode45(PVI1,x,[alfa,0]'); %Primer pvi
-[x,Y2]=ode45(PVI2,x,[0,1]');
+    % Se pueden definir los dos problemas de valor incial dentro de esta
+    % propia función, pero de esta manera no sería genérica.
+    %PVI1 = @(x,u)[u(2);-2./x*u(2) +2./x.^2*u(1)+sin(log(x))./x.^2];
+    %PVI2 = @(r,x)[x(2) ;-2./r*x(2) ];
 
-y=Y1(:,1)+((beta-Y1(end,1))/Y2(end,1))*Y2(:,1);
+    [x,Y1]=ode45(PVI1,x,[alfa,0]'); %Primer pvi
+    [x,Y2]=ode45(PVI2,x,[0,1]');
+
+    y=Y1(:,1)+((beta-Y1(end,1))/Y2(end,1))*Y2(:,1);
 end
--- metodos-numericos-2/Lineal/EjecutableLineal.m
+++ metodos-numericos-2/Lineal/EjecutableLineal.m
@@ -1,19 +1,21 @@
 %Ejecutable: Ejemplo 6
 digits(6)
+a = 1; b = 2; n = 10; alpha = 1; beta = 2;
 
-[xi,yi,Y1,Y2] = DisparoLineal('pvi1','pvi2',1 ,2 ,10 ,1 ,2)
+[xi,yi,Y1,Y2] = DisparoLineal('pvi1','pvi2',a ,b ,n ,alpha ,beta)
+
 [xi,yi,Y1(:,1),Y2(:,1)]
 
 
 
-%%
-c1=1/70*(69+4*cos(log(2))+12*sin(log(2)));
-c2=4/35-2/35*cos(log(2))-6/35*sin(log(2));
-yex=@(x) c1.*x+(c2./(x.^2))-3/10.*sin(log(x))-1/10.*cos(log(x));
-%%
-exacta=yex(xi);
+%% Solución exacta
+c1 = 1/70*(69+4*cos(log(2))+12*sin(log(2)));
+c2 = 4/35-2/35*cos(log(2))-6/35*sin(log(2));
+yex = @(x) c1.*x+(c2./(x.^2))-3/10.*sin(log(x))-1/10.*cos(log(x));
+exacta = yex(xi);
 
-Error=abs(yi-exacta);
+%% Cálculo del error
+Error = abs(yi-exacta);
 format short e
 [xi,yi,exacta,Error]
 
@@ -22,4 +24,6 @@ vpa(Error)
 %%
 hold on
 plot(xi,yi,'*-r')
+plot(xi,Y1(:,1),'k')
+plot(xi,Y2(:,1),'k')
 plot(xi,exacta,'b')
