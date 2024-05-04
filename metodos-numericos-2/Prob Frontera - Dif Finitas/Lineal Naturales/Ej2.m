02a500c8f92a4ef99a4dc6f80014d9c00abb3bff
Pablo Ruiz <pablo.ruiz.molina@gmail.com>
2024-05-04 14:13:04 +0200

    MN-2 Tema 3-2

--- metodos-numericos-2/Prob Frontera - Dif Finitas/Lineal Naturales/Ej2.m
+++ metodos-numericos-2/Prob Frontera - Dif Finitas/Lineal Naturales/Ej2.m
@@ -1,14 +1,31 @@
-alfa = 1 -1/(2* log (3));
-beta = 0.5 -1/(6* log (3));
-a=1;
-b=3;
-N=9;
-[ri,yi] = DiFiLinealEj2 (a,b,alfa ,beta ,N)
-%%
+%%% Ejemplo 1 %%%
+clear all; close all;
+
+%% Se establecen los parámetros inciales
+alfa = 1-1/(2*log(3));
+beta = 0.5-1/(6*log(3));
+a = 1; b = 3; N = 9; % N + 1 intervalos -> N + 2 nodos
+
+%% Se ejecuta el método de Diferencias Finitas para un problema líneal con condiciones Dirichlet
+[ri,yi] = DiFiLinealEj2(a, b, alfa, beta, N);
+
+%% Se calcula la solución exacta
 ex=@(r)1./(log(1/3)).*(log(r/3)-0.5.*log(r));
-exacta=ex(ri);
-Error=abs(exacta-yi);
-[ri,yi,exacta,Error]
+
+%% Se evalua la solución exacta en xi y se calcula el Error
+exacta = ex(ri);
+digits(8)
+Error = vpa(abs(exacta-yi),6);
+
+%% Se imprimen por consola los resultados en precisión aritmética
+[vpa(ri),vpa(yi),vpa(exacta),vpa(Error)]
+
+%% Se representan las dos soluciones
 hold on
-plot(ri,yi,'r-*')
+grid on
+title("problema de frontera líneal - Dirichlet")
+xlabel('r');
+ylabel('u(r)');
+plot(ri,yi,'*-r')
 plot(ri,exacta,'b')
+legend('u(r) aproximada', 'u(r) exacta');
