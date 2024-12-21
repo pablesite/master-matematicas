4c2a2c5fe14b6832148b01901c88af9e5051abb4
Pablo Ruiz <pablo.ruiz.molina@gmail.com>
2024-12-21 23:51:46 +0100

    Actualización de varias asignaturas

--- optimizacion/Actividad 1/rein.m
+++ optimizacion/Actividad 1/rein.m
@@ -1,5 +1,4 @@
 function [x_min, iter] = rein(f, x0, alpha, beta, eps)
-    % METODO_RECTAS_GOLDSTEIN
     % Método de las rectas inexactas con las condiciones de Armijo y Goldstein.
     %
     % Entradas:
@@ -16,32 +15,36 @@ function [x_min, iter] = rein(f, x0, alpha, beta, eps)
     % Inicialización
     iter = 0;
     xk = x0; % Punto inicial
-    h = 1/1000;
     max_iter = 100;
-    grad = 1; % para empezar
+    grad_lambda_d = 1; % Para no cumplir el criterio de parada
 
-    while abs(grad) > eps && iter < max_iter
+    % Paso de la señal. -> Aproximar gradiente
+    h = 1/1000; % Tamaño de paso 
+    
+    while abs(grad_lambda_d) > eps && iter < max_iter
      
-        % Calculo de grad(f(xk))
-        fx_2h = f(xk + 2*h); fx_h = f(xk + h); fx_nh = f(xk - h); fx_2nh = f(xk - 2*h);
-        grad = (-fx_2h + 8*fx_h - 8*fx_nh + fx_2nh)/(12*h);
+        % Cálculo de grad(f(xk)) mediante aproximación de diferencias
+        % finitas
+        %fx_2h = f(xk + 2*h); fx_h = f(xk + h); fx_nh = f(xk - h); fx_2nh = f(xk - 2*h);
+        %grad = (-fx_2h + 8*fx_h - 8*fx_nh + fx_2nh)/(12*h);
+        grad = grad_lambda_d;
 
-        % Calculo dirección
+        % Cálculo de dirección
         d = -grad;
 
-        % Para cada salto, empiezo considerando lambda = 1
+        % Para cada salto, se empieza considerando lambda = 1
         lambda = 1;
 
         % Evalúo la función en xk y xk + lambda_d
         fxk = f(xk);
         fxk_lambda_d = f(xk + lambda*d);
         
-        % Calculo grad(f(xk + lambda*d))
+        % Cálculo de grad(f(xk + lambda*d))
         fx_2h_lambda_d = f(xk + lambda*d + 2*h); fx_h_lambda_d = f(xk + lambda*d + h); fx_nh_lambda_d = f(xk + lambda*d - h); fx_2nh_lambda_d = f(xk + lambda*d - 2*h);
         grad_lambda_d = (-fx_2h_lambda_d + 8*fx_h_lambda_d - 8*fx_nh_lambda_d + fx_2nh_lambda_d)/(12*h);
 
    
-        while (fxk_lambda_d > fxk + alpha*lambda*grad*d) || beta*grad*d > grad_lambda_d*d
+        while ((fxk_lambda_d > fxk + alpha*lambda*grad*d) || (beta*grad*d > grad_lambda_d*d))
             % Actualizo lambda
             lambda = 0.5*lambda;
             
@@ -62,13 +65,7 @@ function [x_min, iter] = rein(f, x0, alpha, beta, eps)
         xk = xk + lambda*d;
         iter = iter + 1;
 
-        % Evalúo gradiente de f(xk) para revisar condición de parada
-        fx_2h = f(xk + 2*h); fx_h = f(xk + h); fx_nh = f(xk - h);fx_2nh = f(xk - 2*h);
-        grad = (-fx_2h + 8*fx_h - 8*fx_nh + fx_2nh)/(12*h);
-
     end
-    
-    % Resultados
+    % Mínimo
     x_min = xk;
-    %f_min = f(xk);
 end
---
+++ optimizacion/Newton_Multi.m
@@ -0,0 +1,32 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                  ALGORITMO DE NEWTON - DOS VARIABLES                %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PROF. MARLON MOSCOSO %%
+%% Considere la función: f(X1,X2)=(X1-2)^4+(X1-2)^2*X2^2+(X2+1)^2      %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Calculo del Gradiente de f - Vector de Derivadas Parciales
+Df = @(x) [(4*(x(1)-2)^3 + 2*(x(1)-2)*x(2)^2);
+             (2*x(2)*(x(1)-2)^2+2*(x(2)+1))];
+%% Cálculo de la Matriz Hessiana - Matriz xe segunda derivadas parciales
+DDf = @(x) [(12*(x(1)-2)^2+2*x(2)^2)  (4*(x(1)-2)*x(2)); 
+            (4*(x(1)-2)*x(2))  (2*(x(1)-2)^2+2)];
+%% Parámetros de Inicio del Algoritmo
+x0 =[1,1];                       % Iterado Inicial - Punto de Inicio
+maxiter=10;                      % Número máximo de iteraciones 
+tol =1E-12;                      % Tolerancia pedida - Criterio de Parada
+x=x0;                            % Inicio de las Variables
+iter =0;                         % Contador de iteraciones
+%% Ciclo para generar los iterados
+while iter < maxiter
+D=Df(x);                % Vector Gradiente
+n1= sqrt(D'*D);         % Norma del Vector Gradiente - Criterio de Parada
+       % Se imprime cada iterado y el error que se comete en la iteración
+sprintf (" Iterado %d, aprox=( %f, %f), error= %e \n", iter , x, n1)
+h  = DDf(x)\D;            % Cálculo del p correspondiente          
+n2 = sqrt(h'*h);         % Norma de p - Segundo Criterio de Parada
+x  = x-h';                 % Actualización del punto x=(x(1),x(2))
+if (n1 < tol) || (n2 < tol) % Pregunta para Criterio de Parada
+iter = maxiter ;
+break;
+end
+iter = iter +1;         % Actualización del Contador
+end
